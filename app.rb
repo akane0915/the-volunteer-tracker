c6933676f4c7652f972f733c0ea6fe4e992f605a
Asia Kane <asialkane@gmail.com>
2017-05-05 12:02:07 -0700

    add routing for app.rb and erb files for index, project, and layout

--- app.rb
+++ app.rb
@@ -6,6 +6,34 @@ also_reload('lib/**/*.rb')
 require 'pry'
 require 'pg'
 
-get('/') do
-  erb(:index)
+DB = PG.connect({:dbname => 'volunteer_tracker_test'})
+
+get '/' do
+  @projects = Project.all
+  erb :index
+end
+
+post '/' do
+  title = params.fetch('title')
+  new_project = Project.new({:title => title, :id => nil})
+  new_project.save
+  @projects = Project.all
+  erb :index
+end
+
+get '/projects/:id' do
+  id = params.fetch('id').to_i
+  @project = Project.find(id)
+  erb :project
+end
+
+post '/projects/:id' do
+  # @id = params.fetch('id').to_i
+  name = params.fetch('name')
+  hours = params.fetch('hours').to_i
+  project_id = params.fetch('project_id').to_i
+  new_volunteer = Volunteer.new({:name => name, :hours => hours, :project_id => project_id})
+  new_volunteer.save
+  @project = Project.find(project_id)
+  erb :project
 end
