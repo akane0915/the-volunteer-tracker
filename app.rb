9832d658718808408457f4e0e991d6c184563a9e
Asia Kane <asialkane@gmail.com>
2017-05-05 13:46:17 -0700

    add routing and erb pages to update and delete projects

--- app.rb
+++ app.rb
@@ -13,6 +13,7 @@ get '/' do
   erb :index
 end
 
+# post '/projects' do
 post '/' do
   title = params.fetch('title')
   new_project = Project.new({:title => title, :id => nil})
@@ -27,6 +28,7 @@ get '/projects/:id' do
   erb :project
 end
 
+# post '/volunteers' do
 post '/projects/:id' do
   # @id = params.fetch('id').to_i
   name = params.fetch('name')
@@ -36,4 +38,25 @@ post '/projects/:id' do
   new_volunteer.save
   @project = Project.find(project_id)
   erb :project
+end
+
+get '/projects/:id/edit' do
+  @id = params.fetch('id').to_i
+  @project = Project.find(@id)
+  erb :project_edit
+end
+
+patch '/projects/:id' do
+  new_title = params.fetch('title')
+  @id = params.fetch('id').to_i
+  @project = Project.find(@id)
+  @project.update({:title => new_title, :id => @id})
+  erb :project
+end
+
+delete'/projects/:id' do
+  @id = params.fetch('id').to_i
+  Project.find(@id).delete
+  @projects = Project.all
+  erb :index
 end
