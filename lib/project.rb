14d6ce9c49b633c01e934f020ea4073b3240bdc9
Asia Kane <asialkane@gmail.com>
2017-05-05 10:37:30 -0700

    add passing tests and methods to save projects and return all projects

--- lib/project.rb
+++ lib/project.rb
@@ -5,4 +5,24 @@ class Project
     @title = attributes.fetch(:title)
     @id = attributes.fetch(:id)
   end
+
+  def ==(another_project)
+    @title == another_project.title
+  end
+
+  def Project.all
+    projects = []
+    db_output = DB.exec('SELECT * from projects;')
+    db_output.each do |project|
+      title = project.fetch('title')
+      id = project.fetch('id').to_i
+      projects.push(Project.new({:title => title, :id => id}))
+    end
+    projects
+  end
+
+  def save
+    returned_id = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+    @id = returned_id.first.fetch('id').to_i
+  end
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -6,17 +6,39 @@ describe Project do
       test_project = Project.new({:title => 'Bellview Elementary School Garden Planting', :id => nil})
       expect(test_project.title).to eq 'Bellview Elementary School Garden Planting'
     end
+  end
 
-    describe '#id' do
-      it 'returns the id of the project before saving project' do
-        test_project = Project.new({:title => 'Bellview Elementary School Garden Planting', :id => nil})
-        expect(test_project.id).to eq nil
-      end
-      it 'returns the id of the project after saving project' do
-        test_project = Project.new({:title => 'Bellview Elementary School Garden Planting', :id => nil})
-        test_project.save
-        expect(test_project.id).to be_an_instance_of Fixnum
-      end
+  describe '#id' do
+    it 'returns the id of the project before saving project' do
+      test_project = Project.new({:title => 'Bellview Elementary School Garden Planting', :id => nil})
+      expect(test_project.id).to eq nil
+    end
+    it 'returns the id of the project after saving project' do
+      test_project = Project.new({:title => 'Bellview Elementary School Garden Planting', :id => nil})
+      test_project.save
+      expect(test_project.id).to be_an_instance_of Fixnum
+    end
+  end
+
+  describe '#==' do
+    it 'is the same project if two projects have the same title' do
+      project1 = Project.new({:title => 'Bellview Elementary School Garden Planting', :id => nil})
+      project2 = Project.new({:title => 'Bellview Elementary School Garden Planting', :id => nil})
+      expect(project1 == project2).to eq true
+    end
+  end
+
+  describe '.all' do
+    it 'is empty to start' do
+      expect(Project.all).to eq []
+    end
+
+    it 'returns all projects alphabetically' do
+      project1 = Project.new({:title => 'Bellview Elementary School Garden Planting', :id => nil})
+      project1.save
+      project2 = Project.new({:title => 'Habitat for Humanity Build', :id => nil})
+      project2.save
+      expect(Project.all).to eq [project1, project2]
     end
   end
 end
