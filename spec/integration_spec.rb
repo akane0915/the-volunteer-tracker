184fcf1a08d7c8cba6ff5f0bc873ac55be9a50ec
Asia Kane <asialkane@gmail.com>
2017-05-05 16:17:56 -0700

    add three user integration tests for homepage, project creation, and volunteer creation

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,17 +1,36 @@
 require "capybara/rspec"
 require "./app"
+require "pry"
+require('spec_helper')
 
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-# example integration test
+describe 'the homepage display path', {:type => :feature} do
+  it 'shows a welcome message' do
+    visit '/'
+    expect(page).to have_content "Welcome to BetterTheWorld's Volunteer Tracker"
+  end
+end
 
-# describe("the phrase parser path", {:type => :feature}) do
-#   it("processes the user input and returns correct message if its a palindrome") do
-#     visit("/")
-#     fill_in("phrase1", :with => "madam")
-#     fill_in("phrase2", :with => "anagram")
-#     click_button("what am i?")
-#     expect(page).to have_content("'madam' is a palindrome")
-#   end
-# end
+describe 'the project creation path', {:type => :feature} do
+  it 'takes the user to the homepage where they can create a project' do
+    visit '/'
+    fill_in('title', :with => 'Bellview Elementary School Garden Planting')
+    click_button('Create Project')
+    expect(page).to have_content('Bellview Elementary School Garden Planting')
+  end
+end
+
+describe 'the volunteer creation path', {:type => :feature} do
+  it 'adds a volunteer to a project' do
+    test_project = Project.new({:title => 'Bellview Elementary School Garden Planting', :id => nil})
+    test_project.save
+    visit '/'
+    select('Bellview Elementary School Garden Planting', :from => 'project_id')
+    fill_in('name', :with => 'Sally Jenkins')
+    fill_in('hours', :with => 0)
+    click_button('Add Volunteer')
+    expect(page).to have_content('Sally Jenkins')
+  end
+end
