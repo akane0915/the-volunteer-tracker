e8ea38b853db3f77f77a6225cbd54871cb43ea39
Asia Kane <asialkane@gmail.com>
2017-05-05 16:43:59 -0700

    add user integration specs to test the project update and delete path

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -22,7 +22,31 @@ describe 'the project creation path', {:type => :feature} do
   end
 end
 
-describe 'the volunteer creation path', {:type => :feature} do
+describe 'the project update path', {:type => :feature} do
+  it 'takes the allows a user to change the name of the project' do
+    test_project = Project.new({:title => 'Bellview Elementary School Garden Planting', :id => nil})
+    test_project.save
+    visit '/'
+    click_link('Bellview Elementary School Garden Planting')
+    click_link('Edit Bellview Elementary School Garden Planting Project')
+    fill_in('title', :with => 'Walker Elementary School Garden Planting')
+    click_button('Update Project')
+    expect(page).to have_content('Walker Elementary School Garden Planting')
+  end
+end
+
+describe 'the project delete path', {:type => :feature} do
+  it 'allows a user to delete a project' do
+    test_project = Project.new({:title => 'Bellview Elementary School Garden Planting', :id => nil})
+    test_project.save
+    id = test_project.id
+    visit "/projects/#{id}/edit"
+    click_button('Delete Project')
+    expect(page).to have_content("Welcome to BetterTheWorld's Volunteer Tracker")
+  end
+end
+
+describe 'the volunteer creation and deletion path', {:type => :feature} do
   it 'adds a volunteer to a project' do
     test_project = Project.new({:title => 'Bellview Elementary School Garden Planting', :id => nil})
     test_project.save
@@ -33,4 +57,9 @@ describe 'the volunteer creation path', {:type => :feature} do
     click_button('Add Volunteer')
     expect(page).to have_content('Sally Jenkins')
   end
+  # it 'deletes a volunteer' do
+  #   click_link('Sally Jenkins')
+  #   click_button('Delete Volunteer')
+  #   expect(page).to have_content('Bellview Elementary School Garden Planting')
+  # end
 end
