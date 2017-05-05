e6d42ab99c4ce9a7b64b61d65880ae695d1b95ac
Asia Kane <asialkane@gmail.com>
2017-05-05 09:46:15 -0700

    add passing tests to return volunteer hours and project_id.

--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -7,4 +7,18 @@ describe Volunteer do
       expect(test_volunteer.name).to eq 'Sally'
     end
   end
+
+  describe '#hours' do
+    it 'returns the hours of the volunteer' do
+      test_volunteer = Volunteer.new({:name => 'Sally', :hours => 10, :project_id => 1})
+      expect(test_volunteer.hours).to eq 10
+    end
+  end
+
+  describe '#project_id' do
+    it 'returns the project_id of the volunteer' do
+      test_volunteer = Volunteer.new({:name => 'Sally', :hours => 10, :project_id => 1})
+      expect(test_volunteer.project_id).to eq 1
+    end
+  end
 end
