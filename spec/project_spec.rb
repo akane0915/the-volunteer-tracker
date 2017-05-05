090a6e468312c46bc33047b70ec01e39fc950aa3
Asia Kane <asialkane@gmail.com>
2017-05-05 10:20:36 -0700

    add two passing tests and initialize method for project class.

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,1 +1,22 @@
 require "spec_helper"
+
+describe Project do
+  describe '#title' do
+    it 'returns the title of the project' do
+      test_project = Project.new({:title => 'Bellview Elementary School Garden Planting', :id => nil})
+      expect(test_project.title).to eq 'Bellview Elementary School Garden Planting'
+    end
+
+    describe '#id' do
+      it 'returns the id of the project before saving project' do
+        test_project = Project.new({:title => 'Bellview Elementary School Garden Planting', :id => nil})
+        expect(test_project.id).to eq nil
+      end
+      it 'returns the id of the project after saving project' do
+        test_project = Project.new({:title => 'Bellview Elementary School Garden Planting', :id => nil})
+        test_project.save
+        expect(test_project.id).to be_an_instance_of Fixnum
+      end
+    end
+  end
+end
